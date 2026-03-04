0dc11f3603592fe327ec576ce8bfee9aee16da78
abbi portwood <apwood09@gmail.com>
2026-03-03 23:03:36 -0500

    wrote code for 5 fuctions to manage budgets; functions passed 21 test

--- index.js
+++ index.js
@@ -1,7 +1,29 @@
+function calculateTax(amount) {
+    const taxRate = 0.10; // 10%
+    return amount * taxRate; 
+}
 
+function convertToUpperCase(text) {
+     return text.toUpperCase();
+}
 
+function findMaximum(num1, num2) {
+    return (num1 > num2) ? num1 : num2; 
+}
 
+function isPalindrome(word) {
+    const processedWord = word.toLowerCase().replace(/[^a-z0-9]/g, '');
+    const reversedWord = processedWord.split('').reverse().join('');
+    return processedWord === reversedWord;
+}
 
+function calculateDiscountedPrice(originalPrice, discountPercentage) {
+    const discountAmount = (originalPrice * discountPercentage) / 100;
+
+    const discountedPrice = originalPrice - discountAmount;
+  
+    return discountedPrice;
+}
 
 // This is required for the test to function properly  
 module.exports = { calculateTax, convertToUpperCase, findMaximum, isPalindrome, calculateDiscountedPrice };
